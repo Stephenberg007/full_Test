aa78cbfa12cc3b1646cafd7465ddcac405124f01
Stephenberg007 <krmauryaaman@gmail.com>
2024-11-27 23:05:24 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,15 +8,13 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-
+		int a =100;
 
 		int i=10;
 		float i =22.45f;
 double price = 1000.0;
 boolean b1 = true;
 		boolean isUpdated = false;
-
-		
 		system.out.println(i);
 
 
