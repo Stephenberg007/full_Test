c70a5260bc68de08302ae4f538ac2174a42f4cdd
Aman <krmauryaaman@gmail.com>
2024-11-28 12:12:15 +0530

    jira-102

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+// JIRA-102
+		void m2(){
+			//logic
+		}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		int a =100;
