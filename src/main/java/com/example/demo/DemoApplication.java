e2d90b78ecd364771cda0fe0011c6455527b06e6
Aman <krmauryaaman@gmail.com>
2024-11-28 12:17:54 +0530

    1st

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-	
+	//101
+	void m1(){
+		System.out.println(101);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
