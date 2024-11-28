761493e79d060e898ed16cae9866e77fdabb6a1c
Stephenberg007 <krmauryaaman@gmail.com>
2024-11-28 13:10:27 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	
-
+System.out.println("1st Line");
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		int a =100;
